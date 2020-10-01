abd7cf7fe33b060ff3527864bbd8285d20f7541f
leo <leonardo.pellizzoni@gmail.com>
2020-09-30 22:08:56 -0300

    Dropout

---
+++ 464DropoutFromScratch.py
@@ -0,0 +1,56 @@
+from d2l import mxnet as d2l
+from mxnet import autograd, gluon, init, np, npx
+from mxnet.gluon import nn
+npx.set_np()
+
+def dropout_layer(X, dropout):
+    assert 0 <= dropout <= 1
+    # In this case, all elements are dropped out
+    if dropout == 1:
+        return np.zeros_like(X)
+    # In this case, all elements are kept
+    if dropout == 0:
+        return X
+    mask = np.random.uniform(0, 1, X.shape) > dropout
+    return mask.astype(np.float32) * X / (1.0 - dropout)
+
+X = np.arange(16).reshape(2, 8)
+print(dropout_layer(X, 0))
+print(dropout_layer(X, 0.5))
+print(dropout_layer(X, 1))
+
+num_inputs, num_outputs, num_hiddens1, num_hiddens2 = 784, 10, 256, 256
+
+W1 = np.random.normal(scale=0.01, size=(num_inputs, num_hiddens1))
+b1 = np.zeros(num_hiddens1)
+W2 = np.random.normal(scale=0.01, size=(num_hiddens1, num_hiddens2))
+b2 = np.zeros(num_hiddens2)
+W3 = np.random.normal(scale=0.01, size=(num_hiddens2, num_outputs))
+b3 = np.zeros(num_outputs)
+
+params = [W1, b1, W2, b2, W3, b3]
+for param in params:
+    param.attach_grad()
+
+dropout1, dropout2 = 0.2, 0.5
+
+def net(X):
+    X = X.reshape(-1, num_inputs)
+    H1 = npx.relu(np.dot(X, W1) + b1)
+    # Use dropout only when training the model
+    if autograd.is_training():
+        # Add a dropout layer after the first fully connected layer
+        H1 = dropout_layer(H1, dropout1)
+    H2 = npx.relu(np.dot(H1, W2) + b2)
+    if autograd.is_training():
+        # Add a dropout layer after the second fully connected layer
+        H2 = dropout_layer(H2, dropout2)
+    return np.dot(H2, W3) + b3
+
+num_epochs, lr, batch_size = 10, 0.5, 256
+loss = gluon.loss.SoftmaxCrossEntropyLoss()
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+d2l.train_ch3(net, train_iter, test_iter, loss, num_epochs,
+              lambda batch_size: d2l.sgd(params, lr, batch_size))
+
+d2l.plt.show()
